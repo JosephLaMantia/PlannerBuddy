ef83b22da687b7da605e7e40d261bb0bf82b0d19
JosephLaMantia <joeylamaxd@gmail.com>
2022-11-06 16:32:43 -0500

    basic functionality to add, remove, and edit tasks. not much styling

--- plannerbuddy/src/App.js
+++ plannerbuddy/src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
+import logo from "./assets/images/Notepad_BOOP.png"
 import './App.css';
+import TodoForm from './components/TodoForm';
+import TodoList from './components/TodoList';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="planner-buddy">
+      <div className='title'>
+        <img className="title-logo" src={logo} alt=""/>
+        <h1 className='title-text'>PlannerBuddy</h1>
+        </div>
+      <TodoList/>
     </div>
   );
 }
